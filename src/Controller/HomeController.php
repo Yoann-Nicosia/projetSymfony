fd79d2d79fe282f63a5bea0b14bd8f318be5280b
Yoann-Nicosia <yoann.nicosia@gmail.com>
2024-02-02 14:19:11 +0100

    push

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,12 +11,8 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        $users = [
-            ['name' => 'John Doe', 'phone' => '0706060606'],
-            ['name' => 'Fred Doe', 'phone' => '0806060606'],
-            ['name' => 'Aypierre Doe', 'phone' => '0906060606']
-        ];
-        return $this->render('home/index.html.twig', compact("users"));
+        
+        return $this->render('home/index.html.twig');
         
     }
     
